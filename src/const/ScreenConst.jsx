bcfdebcb1a87eb0a75239b8a52bece0abe221076
tungjj <tungplatin@gmail.com>
2024-05-15 18:35:32 +0700

    add ScreenConst

---
+++ src/const/ScreenConst.jsx
@@ -0,0 +1,7 @@
+const ScreenConst = {
+  PRACTICING_SCREEN: 'PracticingScreen',
+  ATTENDANCE_SCREEN: 'AttendanceScreen',
+  MEMBER_SCREEN: 'MemberScreen',
+  ADD_PRACTICING_SCREEN: 'AddPracticingScreen',
+};
+export default ScreenConst;
