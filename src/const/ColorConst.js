700fe277f4983ddbb193e9943573e3dc2a812ebf
tungjj <tungplatin@gmail.com>
2024-05-15 18:11:44 +0700

    add color constant

---
+++ src/const/ColorConst.js
@@ -0,0 +1,9 @@
+const ColorConst = {
+  primaryColor: '#50C2C9',
+  secondaryColor: '#F4C395',
+  thirdColor: '#FB2222',
+  white: '#FFFFF',
+  spaceGray: '#E8E8E8',
+};
+
+export default ColorConst;
